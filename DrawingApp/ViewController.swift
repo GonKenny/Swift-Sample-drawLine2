f746d17a0ccc666511a0cf2b0c7ad5b1af810098
GonKenny <takada.ayako@gmail.com>
2015-01-28 13:36:43 +0900

    first commit 150128 13:36

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -1,15 +1,17 @@
-//
-//  ViewController.swift
-//  DrawingApp
-//
-//  Created by gonKenny on 2015/01/23.
-//  Copyright (c) 2015年 gonKenny. All rights reserved.
-//
+//1.クリアボタンを設置→viewController.swiftにつなげる
+//2.新規ファイル→CocoaTouch classのファイル作成（drawView.swift)
+//3.Viewを設置→custom classにdrawViewを入力してつなげる
+//5.swiftファイルを作成(line.swift)
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    //9.描画エリアを接続
+    @IBOutlet weak var drawViewArea: drawView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func claerTapped(sender: UIButton) {      //1.
+        //10.
+        var theDrawView : drawView = drawViewArea as drawView    //
+        theDrawView.lines = []
+        theDrawView.setNeedsDisplay()
+        }
 
 }
-
---
+++ DrawingApp/line.swift
@@ -0,0 +1,14 @@
+//5.
+
+import UIKit
+
+class Line {
+    var start: CGPoint
+    var end: CGPoint
+    
+    init(start _start: CGPoint, end _end:CGPoint){
+        start = _start
+        end = _end
+    }
+
+}
